fcc92387d8fb44872b0956e9990b3319dcfaf98a
Cucina <60839899+iamnovichek@users.noreply.github.com>
2023-03-06 18:23:38 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,32 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .forms import UserLogin
 
 
 class User:
 
-    @staticmethod
-    def home(request):
-
-        return render(request, 'home.html')
-
-    @staticmethod
-    def log_in(request):
-
-        if request.method == 'POST':
-
-            form = UserLogin(request.POST)
-
-            if form.is_valid():
-
-                return HttpResponseRedirect('home')
-
-            else:
-
-                return HttpResponseRedirect('login')
-
-        else:
-
-            form = UserLogin()
-
-        return  render(request, 'login.html', {'form': form})
+    pass
