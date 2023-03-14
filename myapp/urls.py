59aa1da7f9d3d38c00d1cafc5f54b76b804d4f68
iamnovichek <rungotika@gmail.com>
2023-03-14 17:07:15 +0100

    add login app

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('logout/', LogoutView.as_view(), name='logout')
 ]
